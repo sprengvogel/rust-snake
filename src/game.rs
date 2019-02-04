5107c45dcd1e46e6778e56d66ea1a35b37cbe2f2
Niklas Sprengel <niklas.sprengel@campus.tu-berlin.de>
2019-02-05 00:13:09 +0100

    Removes possibility of munchie being spawned in the snake, also refactors a bit

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,10 @@ use piston_window::*;
 use piston_window::types::Color;
 use crate::snake::Snake;
 use crate::drawer::*;
+use crate::WINDOW_WIDTH;
+use crate::WINDOW_HEIGHT;
 use std::{thread, time};
+use rand::Rng;
 
 const MN_RED: f32 = 50.0;
 const MN_GREEN: f32 = 130.0;
@@ -15,22 +18,28 @@ const MUNCHIE_WIDTH: f64 = 0.5;
 
 pub struct Game {
     snake: Snake,
+    munchie: Block,
 }
 impl Game {
     pub fn new() -> Game {
         Game {
-            snake: Snake::new(3,3)
+            snake: Snake::new(3,3),
+            munchie: Block{x:7, y:7}
         }
     }
 
     pub fn draw(&mut self, context: &Context, g: &mut G2d) {
+        thread::sleep(time::Duration::from_millis(100));
+        let munchie_eaten = self.snake.move_snake(&self.munchie);
+        if munchie_eaten {
+            self.generate_new_munchie();
+        }
         self.snake.draw_self(context, g);
         self.draw_munchie(context, g);
-        thread::sleep(time::Duration::from_millis(100));
     }
 
     fn draw_munchie(&self, context: &Context, g: &mut G2d) {
-        draw_small_square(MUNCHIE_COLOR, self.snake.get_munchie().x, self.snake.get_munchie().y, MUNCHIE_WIDTH, context, g);
+        draw_small_square(MUNCHIE_COLOR, self.munchie.x, self.munchie.y, MUNCHIE_WIDTH, context, g);
     }
 
     pub fn key_pressed(&mut self, key: Key) {
@@ -44,6 +53,18 @@ impl Game {
     }
     pub fn is_opposite_direction(dir1: &Direction, dir2: &Direction) -> bool {
         (*dir1==Direction::Left && *dir2==Direction::Right)||(*dir1==Direction::Right && *dir2==Direction::Left)||(*dir1==Direction::Up && *dir2==Direction::Down)||(*dir1==Direction::Down && *dir2==Direction::Up)
+    }
+
+    fn generate_new_munchie(&mut self) {
+        let mut munchie_x = rand::thread_rng().gen_range(0, WINDOW_WIDTH-1);
+        let mut munchie_y = rand::thread_rng().gen_range(0, WINDOW_HEIGHT-1);
+        let mut new_munchie = Block{x: munchie_x,y: munchie_y};
+        while self.snake.check_self_collision(&new_munchie) {
+            munchie_x = rand::thread_rng().gen_range(0, WINDOW_WIDTH-1);
+            munchie_y = rand::thread_rng().gen_range(0, WINDOW_HEIGHT-1);
+            new_munchie = Block{x: munchie_x,y: munchie_y};
+        }
+        self.munchie=new_munchie;
     }
 
     pub fn game_over() {
--- src/snake.rs
+++ src/snake.rs
@@ -4,7 +4,6 @@ use crate::game::*;
 use crate::drawer::*;
 use piston_window::*;
 use piston_window::types::Color;
-use rand::Rng;
 
 const SN_RED: f32 = 205.0;
 const SN_GREEN: f32 = 130.0;
@@ -15,7 +14,6 @@ const SNAKE_COLOR: Color = [SN_RED/255.0, SN_GREEN/255.0, SN_BLUE/255.0, SN_ALPH
 pub struct Snake {
     blocks: Vec<Block>,
     direction: Direction,
-    munchie: Block
 }
 
 impl Snake {
@@ -23,19 +21,34 @@ impl Snake {
         Snake {
             blocks: vec!(Block{x:x, y:y},Block{x:x-1, y:y},Block{x:x-2, y:y}),
             direction: Direction::Right,
-            munchie: Block{x:7, y:7}
         }
     }
     ///Moves the snake one block and redraws it
     pub fn draw_self(&mut self, context: &Context, g: &mut G2d) {
-        self.move_snake();
         for block in &mut self.blocks {
             draw_block(SNAKE_COLOR, block.x as f64, block.y as f64, context, g);
         }
     }
 
     ///Moves the snake one block while checking for collisions
-    fn move_snake(&mut self) {
+    pub fn move_snake(&mut self, munchie: &Block) -> bool{
+        let new_head = self.compute_new_head();
+        let game_over=self.check_self_collision(&new_head);
+        if game_over {
+            Game::game_over();
+        }
+        let mut munchie_eaten=false;
+        if new_head == *munchie {
+            //If munchie is eaten, dont pop end to get bigger and generate new munchie
+            munchie_eaten=true;
+        } else {
+            self.blocks.pop();
+        }
+        self.blocks.insert(0, new_head);
+        munchie_eaten
+    }
+
+    fn compute_new_head(&self) -> Block {
         let old_head = &self.blocks[0];
         let mut new_head = Block{x:old_head.x, y:old_head.y};
         match self.direction {
@@ -68,28 +81,16 @@ impl Snake {
                 }
             }
         }
-        self.check_self_collision(&new_head);
-        if new_head == self.munchie {
-            //If munchie is eaten, dont pop end to get bigger and generate new munchie
-            let munchie_x = rand::thread_rng().gen_range(0, WINDOW_WIDTH-1);
-            let munchie_y = rand::thread_rng().gen_range(0, WINDOW_HEIGHT-1);
-            self.munchie=Block{x: munchie_x,y: munchie_y};
-        } else {
-            self.blocks.pop();
-        }
-        self.blocks.insert(0, new_head);
+        new_head
     }
+
     pub fn set_direction(&mut self, direction: Direction) {
         if !Game::is_opposite_direction(&self.direction, &direction) {
             self.direction = direction;
         }
     }
-    pub fn get_munchie(&self) -> &Block {
-        &self.munchie
-    }
-    fn check_self_collision(&self, new_head: &Block) {
-        if self.blocks.contains(new_head) {
-            Game::game_over();
-        }
+    
+    pub fn check_self_collision(&self, block: &Block) -> bool{
+        self.blocks.contains(block)
     }
 }
